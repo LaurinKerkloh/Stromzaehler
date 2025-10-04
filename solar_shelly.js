bd51968316ba55b5576f83a71ec6842e281b1c7f
Laurin Kerkloh <laurin@kerk-loh.de>
2025-10-04 14:48:06 +0200

    update shelly script

--- solar_shelly.js
+++ solar_shelly.js
@@ -3,26 +3,21 @@ const topic = "tele/Solar/SENSOR";
 
 Timer.set(interval, true, function () {
   // Get EM data
-  let emData = Shelly.getComponentStatus("em", 0);
+  let em = Shelly.getComponentStatus("em", 0);
+  let emData = Shelly.getComponentStatus("emdata", 0);
 
-  if (emData) {
-    let totalPower = emData.total_act_power; // Total active power (W)
-    let totalEnergy = emData.total; // Total energy consumed (Wh)
+  let totalPower = em.total_act_power; // Total active power (W)
+  let totalEnergy = emData.total_act; // Total energy consumed (Wh)
 
-    // Create JSON payload
-    let payload = JSON.stringify({
-      Time: new Date().toISOString(),
-      Solar: {
-        reading: totalEnergy,
-        current_power: totalPower,
-      },
-    });
+  // Create JSON payload
+  let payload = JSON.stringify({
+    Time: new Date().toISOString(),
+    Solar: {
+      reading: totalEnergy,
+      current_power: totalPower,
+    },
+  });
 
-    // Publish to MQTT
-    MQTT.publish("topic", payload, 0, false);
-
-    console.log("MQTT ->", topic, payload);
-  } else {
-    console.log("Error: Unable to read EM data");
-  }
+  // Publish to MQTT
+  MQTT.publish(topic, payload, 0, false);
 });
