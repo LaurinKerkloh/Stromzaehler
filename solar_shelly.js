8fccf4a5ad654faeb7f92c0a3302fcfa2f39aef8
Laurin Kerkloh <laurin@kerk-loh.de>
2025-10-09 09:21:09 +0200

    correct message format and better timing

--- solar_shelly.js
+++ solar_shelly.js
@@ -1,23 +1,29 @@
-const interval = 10000;
+//every 10 seconds
+const interval = 10 * 1000;
 const topic = "tele/Solar/SENSOR";
 
-Timer.set(interval, true, function () {
-  // Get EM data
-  let em = Shelly.getComponentStatus("em", 0);
-  let emData = Shelly.getComponentStatus("emdata", 0);
+// wait to start timer on seconds % 10 = 0
+const seconds_to_wait_for = 10 - (new Date().getSeconds() % 10);
+Timer.set(seconds_to_wait_for * 1000, false, function () {
+  // each interval send mqtt data
+  Timer.set(interval, true, function () {
+    // Get EM data
+    const em = Shelly.getComponentStatus("em", 0);
+    const emData = Shelly.getComponentStatus("emdata", 0);
 
-  let totalPower = em.total_act_power; // Total active power (W)
-  let totalEnergy = emData.total_act; // Total energy consumed (Wh)
+    const totalPower = em.total_act_power; // Total active power (W)
+    const totalEnergy = emData.total_act / 1000; // Total energy consumed (kWh)
 
-  // Create JSON payload
-  let payload = JSON.stringify({
-    Time: new Date().toISOString(),
-    Solar: {
-      reading: totalEnergy,
-      current_power: totalPower,
-    },
+    // Create JSON payload
+    let payload = JSON.stringify({
+      Time: new Date().toISOString().slice(0, 19),
+      Solar: {
+        reading: totalEnergy.toFixed(4),
+        current_power: Math.round(totalPower),
+      },
+    });
+
+    // Publish to MQTT
+    MQTT.publish(topic, payload, 0, false);
   });
-
-  // Publish to MQTT
-  MQTT.publish(topic, payload, 0, false);
 });
